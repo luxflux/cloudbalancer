167fbdc77ee62f9aa4533eeba500e0a1a8660f95
Raffael Schmid <raf@nine.ch>
2012-04-17 21:55:58 +0200

    add spec for Node

--- spec/cloud_balancer/node_spec.rb
+++ spec/cloud_balancer/node_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe CloudBalancer::Node do
 
+  before do
+    @node = CloudBalancer::Node.new
+  end
+
+  it "registers with the loadbalancer" do
+    @node.transport.expects(:publish).with(:register, instance_of(Hash)).returns(true)
+    @node.start.should be_true
+  end
+
 end
