6dc932fdcf8d75c7bcc70b75c1b448f7ba996226
Raffael Schmid <raf@nine.ch>
2012-04-17 21:39:59 +0200

    enable registering at the loadbalancer

--- lib/cloud_balancer/node.rb
+++ lib/cloud_balancer/node.rb
@@ -1,15 +1,35 @@
 module CloudBalancer
   class Node
 
+    attr_accessor :transport, :logger
+
     def initialize
       @config = CloudBalancer::Config.node
+
+      @logger = CloudBalancer::Logger.new.logger
+
     end
 
-    def greet_loadbalancer
-      greeting = {
-        password: @config.password,
+    def start
+      register_with_loadbalancer
+    end
+
+    def register_with_loadbalancer
+      register = {
+        password: @config.cluster_password,
         services: @config.services
       }
+      publish(:register, register)
+    end
+
+    def handle_message(metadata, payload)
+      @logger.info("Got message: #{metadata.inspect} - #{payload.inspect}")
+    end
+
+    protected
+
+    def publish(type, data)
+      @transport.publish(type, data)
     end
 
   end
