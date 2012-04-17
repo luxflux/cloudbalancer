c2e7ad949da611c42279c5f8387bf0581d2a85d9
Raffael Schmid <raf@nine.ch>
2012-04-17 21:38:49 +0200

    fix config for Node

--- config/config.rb
+++ config/config.rb
@@ -1,5 +1,5 @@
 configatron.cluster_password = ""
-configatron.server.services = [ ]
+configatron.services = [ :www ]
 configatron.amqp.host = 'localhost'
 configatron.daemon = :load_balancer
 configatron.protocol = :AMQP
@@ -8,5 +8,7 @@ if File.exists?("config/config.local.rb")
   require "./config/config.local"
 end
 
-configatron.load_balancer.cluster_password = Configatron::Dynamic.new {configatron.cluster_password}
-configatron.node.cluster_password = Configatron::Dynamic.new {configatron.cluster_password}
+configatron.load_balancer.cluster_password = Configatron::Dynamic.new { configatron.cluster_password }
+configatron.load_balancer.services = Configatron::Dynamic.new { configatron.services }
+configatron.node.cluster_password = Configatron::Dynamic.new { configatron.cluster_password }
+configatron.node.services = Configatron::Dynamic.new { configatron.services }
