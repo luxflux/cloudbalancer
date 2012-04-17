50776bbf8d524218c302fd4d06488edc4442f1d5
Raffael Schmid <raf@nine.ch>
2012-04-17 21:51:07 +0200

    fix mocking of the amqp connector

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,10 +1,14 @@
 module Helpers
   def mock_amqp_connect
+    exchange = mock
+
     queue = mock
     queue.stubs(:name).returns("generatedstring-123")
     queue.stubs(:subscribe).returns(true)
+    queue.stubs(:bind).with(exchange, instance_of(Hash)).returns(queue)
 
     channel = mock
+    channel.stubs(:topic).with('pub/sub', {:auto_delete => true}).returns(exchange)
     channel.stubs(:queue).with("", :exclusive => true).returns(queue)
 
     ::AMQP.stubs(:connect).returns(true)
